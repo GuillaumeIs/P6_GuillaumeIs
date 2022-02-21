7288cfe2d7759767d178248f73885bf2b8538131
GuillaumeIs <you@example.com>
2022-02-22 00:14:30 +0100

    model User.js

---
+++ back/models/User.js
@@ -0,0 +1,13 @@
+// userSchema : Création du schéma de données utilisateur (User) //
+
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
