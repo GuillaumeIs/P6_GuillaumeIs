0db34be2c8330c479ea318297cb763a0d307e649
GuillaumeIs <you@example.com>
2022-02-22 00:17:11 +0100

    add app

---
+++ back/app.js
@@ -0,0 +1,37 @@
+//Importation Express
+const express = require('express');
+//Importation mongoose (BDD)
+const mongoose = require('mongoose');
+//Importation body-parser (Module pour accèder au corps de la requête)
+const bodyParser = require('body-parser');
+//Importation path (Module chemin de l'image)
+const path = require('path');
+//Importation cors (Module autorisation partage de ressource)
+const cors = require('cors');
+
+//Importation des routes
+const reqSauce = require('./routes/Sauce');
+const reqUser = require('./routes/User');
+
+//Stockage Express dans une variable const
+const app = express();
+
+//Accès à la BDD MongoDB
+mongoose.connect('mongodb+srv://p6user1:p6user1@cluster0.seayg.mongodb.net/Data0?retryWrites=true&w=majority', {
+        useNewUrlParser: true, useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Autorisation partage de ressource
+app.use(cors());
+
+//Accès au corps de la requête
+app.use(bodyParser.json());
+
+//Accès à la route correspondante
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', reqSauce);
+app.use('/api/auth', reqUser);
+
+module.exports = app;
